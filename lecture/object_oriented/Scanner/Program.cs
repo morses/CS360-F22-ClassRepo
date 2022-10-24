d8d7d1c0da63ab2594bf1fc84ae0fb13fa8c4507
Scot Morse <morses@wou.edu>
2022-10-24 11:11:26 -0700

    add a C# console project to demonstrate object oriented style, sorry, forgot to add this last week

---
+++ lecture/object_oriented/Scanner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyProject
+{
+    public class Scanner
+    {
+
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 5;
+            Console.WriteLine(a);
+        }
+    }
+}
